f69e3785b74dcdcb02c9ec35d919d6569504bcf5
write-stuff <100971578+write-stuff@users.noreply.github.com>
2022-10-22 15:47:27 +0530

    Monochrome SSG v01

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# monochrome-ssg
-Custom Static Site Generator for my Website and Blog
+# Monochrome-SSG
+
+## About
+
+A custom-built `Python` Static Site-Generator(SSG) used to generate pages for my personal [website](https://www.abhe.in/) and [blog](https://www.abhe.in/blog/). It's used to fulfill the "back-end" needs of my website hosted here on GutHub Pages.
+
+Called "Monochrome" because it was for being built for a website that was rendered using mostly black and white, and a hint of grey. However, before I could finish this script, I added a dark-mode that's got a lovely shade of [pink](https://www.color-hex.com/color/e62492) as the highlight colour.
+
+The name stuck to the argument that the pink is the only "chrome" in the surrounding greyscale. Though it was mostly because I could not think of anything else.
+
+The code was inspired by [Jahongir Rahmonov's](https://rahmonov.me/posts/static-site-generator/) tutorial.
+
+
+## Python Libraries Used
+
+* `os`
+* `datetime`
+* `markdown2`
+* `jinja2`
+* `tkinter`
+
+## Ugly GUI
+
+The GUI currently looks like this:
+
+!["Monochrome SSG v01 - Ugly GUI"](assets/ugly-gui.png "An ugly GUI of Monochrome SSG v01")
+
+## Future Work
+
+* Add templates for non-blog pages.
+* Add functionality to update templates.
+* Add functionality to regenerate pages with new templates.
+* Add functionality to import `.ipynb` HTML export.
---
+++ monochrome-ssg.py
@@ -0,0 +1,125 @@
+import os
+from datetime import datetime
+from jinja2 import Environment, PackageLoader
+from markdown2 import markdown
+from tkinter import *
+from tkinter import filedialog
+
+label_file_explorer = object
+
+def inputFile():
+    # Asks user to select the source file to be converted. Supports .md and .txt (at least.)
+    global input_filename
+    input_filename = filedialog.askopenfilename(
+        initialdir= "C:\\Users\\Abheshek Pandey\\Documents\\abhe-dot-in\\blog\\content\\",
+        filetypes= (
+            ("All files", "*.*"),
+            ("Markdown files","*.md"),
+            ("Text files", "*.txt")
+        )
+    )
+    label_file_explorer.configure(text="File Opened: "+input_filename)
+
+def newBlog():
+    with open(input_filename, 'r') as file:
+        parsed_md = markdown(file.read(), extras=['metadata'])
+
+        env = Environment(loader=PackageLoader('monochrome', 'templates'))
+        blog_template = env.get_template('blog-template.html')
+
+        data = {
+            'content': parsed_md,
+            'title': parsed_md.metadata['title'],
+            'date': parsed_md.metadata['date']
+        }
+
+    blog_html_content = blog_template.render(post=parsed_md.metadata)
+    blog_filepath = 'C:\\Users\\Abheshek Pandey\\Documents\\abhe-dot-in\\blog\\posts\\' + parsed_md.metadata['slug'] + '.html'
+
+
+    os.makedirs(os.path.dirname(blog_filepath), exist_ok=True)
+    with open(blog_filepath, 'w') as file:
+        file.write(blog_html_content)
+    
+    label_file_explorer.configure(text="Blog Rendered")
+
+def indexUpdate():
+    BLOG = {}
+
+    label_file_explorer.configure(text="Updating index...")
+
+    for blog_md in os.listdir('C:\\Users\\Abheshek Pandey\\Documents\\abhe-dot-in\\blog\\content\\'):
+        file_path = os.path.join('C:\\Users\\Abheshek Pandey\\Documents\\abhe-dot-in\\blog\\content\\', blog_md)
+
+        with open(file_path, 'r') as file:
+            BLOG[blog_md] = markdown(file.read(),extras=['metadata'])
+
+
+    BLOG = {
+        post: BLOG[post] for post in sorted(
+            BLOG, key=lambda post: datetime.strptime(
+                BLOG[post].metadata['date'], '%Y-%m-%d'),
+                reverse=True
+        )
+    }
+
+    env = Environment(loader=PackageLoader('package', 'templates'))
+    all_posts_template = env.get_template('all-posts-template.html')
+    recent_posts_template = env.get_template('recent-posts-template.html')
+
+    index_blog_metadata = [
+        BLOG[post].metadata for post in BLOG
+    ]
+
+    all_posts_html = all_posts_template.render(posts=index_blog_metadata)
+
+    recents = []
+    i=0
+    while i <5:
+        recents.append(index_blog_metadata[i])
+        i = i + 1
+    
+    recent_posts_html = all_posts_template.render(posts=recents)
+
+
+    with open('C:\\Users\\Abheshek Pandey\\Documents\\abhe-dot-in\\blog\\all-posts.html', 'w') as file:
+        file.write(all_posts_html)
+    with open('C:\\Users\\Abheshek Pandey\\Documents\\abhe-dot-in\\blog\\index.html', 'w') as file:
+        file.write(recent_posts_html)
+
+    label_file_explorer.configure(text="Index Updated")
+
+window = Tk()
+window.title('Monochrome SSG v-01')
+window.geometry("500x200")
+window.config(background = "white")
+
+label_file_explorer = Label(window,
+							text = "Please Select the Input file",
+                            width= 60,
+							fg = "black",
+                            bg="white")
+
+button_inputFile = Button(window,
+                            text = "Input File",
+                            command = inputFile)
+
+button_newBlog = Button(window,
+                            text = "Render Blog",
+                            command = newBlog)
+                        
+button_indexUpdate = Button(window,
+                            text = "Update Index",
+                            command = indexUpdate)
+
+button_exit = Button(window,
+                            text = "Exit",
+                            command = exit)
+
+label_file_explorer.grid(column = 1, row = 1)
+button_inputFile.grid(column = 1, row = 2)
+button_newBlog.grid(column = 1, row = 3)
+button_indexUpdate.grid(column = 1, row = 4)
+button_exit.grid(column = 1, row = 5)
+
+window.mainloop()
